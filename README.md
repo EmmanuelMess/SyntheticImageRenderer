204e31a4c40715f80ac14e8c7b90743bb8b2c48a
EmmanuelMess <emmanuelbendavid@gmail.com>
2021-06-05 00:50:15 -0300

    Render more than one image

--- README.md
+++ README.md
@@ -1,6 +1,14 @@
 # SyntheticImageRenderer
 A synthetic image renderer made with Ogre
 
+The code in main generates these files, without opening any windows:
+<img src="rendered.png"/> <img src="rendered2.png"/> <img src="rendered3.png"/>
 
-The code in main generates this file, without opening any windows:
-<img src="rendered.png"/>
+To change the configuration for any image use:
+```c++
+	ProcessConfigurator configurator;
+	configurator.imagesConfigurations.emplace_back(SingleImageConfiguration {
+		.inputMesh = "../fish.mesh", //path to mesh
+		.outputPath = "../rendered.png" //image name
+	});
+```
--- main.cpp
+++ main.cpp
@@ -45,65 +45,99 @@ void loadLocalMesh(const std::string& name, const std::string& path) {
 	meshSerializer.importMesh(stream, mesh.get());
 }
 
-int main() {
-	auto inputMesh = "../fish.mesh";
-	auto outputPath = "../rendered.png";
+struct SingleImageConfiguration {
+	const std::string inputMesh = "../fish.mesh";
+	const std::string outputPath = "../rendered.png";
+};
 
+struct ProcessConfigurator {
+	std::vector<SingleImageConfiguration> imagesConfigurations;
+};
+
+void create(const ProcessConfigurator& configurator) {
 	auto app = SyntheticImageGenerator();
 	app.initApp();
 
 	// get a pointer to the already created root
-	Ogre::Root* root = app.getRoot();
-	Ogre::SceneManager* scnMgr = root->createSceneManager();
+	Ogre::Root *root = app.getRoot();
+	Ogre::SceneManager *scnMgr = root->createSceneManager();
 
 	// register our scene with the RTSS
-	Ogre::RTShader::ShaderGenerator* shadergen = Ogre::RTShader::ShaderGenerator::getSingletonPtr();
+	Ogre::RTShader::ShaderGenerator *shadergen = Ogre::RTShader::ShaderGenerator::getSingletonPtr();
 	shadergen->addSceneManager(scnMgr);
 
 	// without light we would just get a black screen
-	Ogre::Light* light = scnMgr->createLight("MainLight");
-	Ogre::SceneNode* lightNode = scnMgr->getRootSceneNode()->createChildSceneNode();
+	Ogre::Light *light = scnMgr->createLight("MainLight");
+	Ogre::SceneNode *lightNode = scnMgr->getRootSceneNode()->createChildSceneNode();
 	lightNode->setPosition(0, 10, 15);
 	lightNode->attachObject(light);
 
 	// also need to tell where we are
-	Ogre::SceneNode* camNode = scnMgr->getRootSceneNode()->createChildSceneNode();
+	Ogre::SceneNode *camNode = scnMgr->getRootSceneNode()->createChildSceneNode();
 	camNode->setPosition(0, 0, 15);
 	camNode->lookAt(Ogre::Vector3(0, 0, -1), Ogre::Node::TS_PARENT);
 
 	// create the camera
-	Ogre::Camera* cam = scnMgr->createCamera("myCam");
+	Ogre::Camera *cam = scnMgr->createCamera("myCam");
 	cam->setNearClipDistance(5); // specific to this sample
 	cam->setAutoAspectRatio(true);
 	camNode->attachObject(cam);
 
 	// finally something to render
-	auto meshName = "LocalMesh";
-	loadLocalMesh(meshName, inputMesh);
-	Ogre::Entity* ent = scnMgr->createEntity(meshName);
-	Ogre::SceneNode* node = scnMgr->getRootSceneNode()->createChildSceneNode();
-	node->attachObject(ent);
+	Ogre::SceneNode *node = scnMgr->getRootSceneNode()->createChildSceneNode();
 
 	Ogre::TexturePtr rttTexture = Ogre::TextureManager::getSingleton().createManual(
-			"RttTex",
-			Ogre::ResourceGroupManager::DEFAULT_RESOURCE_GROUP_NAME,
-			Ogre::TEX_TYPE_2D,
-			600, 600,
-			0,
-			Ogre::PF_R8G8B8,
-			Ogre::TU_RENDERTARGET);
+		"RttTex",
+		Ogre::ResourceGroupManager::DEFAULT_RESOURCE_GROUP_NAME,
+		Ogre::TEX_TYPE_2D,
+		600, 600,
+		0,
+		Ogre::PF_R8G8B8,
+		Ogre::TU_RENDERTARGET);
 
-	Ogre::RenderTexture* renderTexture = rttTexture->getBuffer()->getRenderTarget();
+	Ogre::RenderTexture *renderTexture = rttTexture->getBuffer()->getRenderTarget();
 
 	renderTexture->addViewport(cam);
 	renderTexture->getViewport(0)->setClearEveryFrame(true);
 	renderTexture->getViewport(0)->setBackgroundColour(Ogre::ColourValue::Black);
 	renderTexture->getViewport(0)->setOverlaysEnabled(false);
 
-	renderTexture->update();
+	for(const auto& image : configurator.imagesConfigurations) {
+		auto meshName = image.inputMesh;
 
-	renderTexture->writeContentsToFile(outputPath);
+		if(!Ogre::MeshManager::getSingleton().resourceExists(meshName, Ogre::ResourceGroupManager::DEFAULT_RESOURCE_GROUP_NAME)) {
+			loadLocalMesh(meshName, image.inputMesh);
+		}
+
+		Ogre::Entity *ent = scnMgr->createEntity(meshName);
+		node->attachObject(ent);
+
+		renderTexture->update();
+
+		renderTexture->writeContentsToFile(image.outputPath);
+
+		node->detachObject(ent);
+		scnMgr->destroyEntity(ent);
+	}
 
 	app.closeApp();
+}
+
+int main() {
+	ProcessConfigurator configurator;
+	configurator.imagesConfigurations.emplace_back(SingleImageConfiguration {
+		.inputMesh = "../fish.mesh",
+		.outputPath = "../rendered.png"
+	});
+	configurator.imagesConfigurations.emplace_back(SingleImageConfiguration {
+		.inputMesh = "../Sword.mesh",
+		.outputPath = "../rendered2.png"
+	});
+	configurator.imagesConfigurations.emplace_back(SingleImageConfiguration {
+		.inputMesh = "../Sinbad.mesh",
+		.outputPath = "../rendered3.png"
+	});
+
+	create(configurator);
 	return 0;
 }
